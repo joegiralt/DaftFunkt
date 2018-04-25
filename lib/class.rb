94e82c4c44ce02d97860675135cd968811a3269f
Joe Giralt <joe.m.giralt@gmail.com>
2018-04-25 16:23:27 -0400

    fix issue with class eval

--- lib/class.rb
+++ lib/class.rb
@@ -1,16 +1,14 @@
 require 'daft_funkt'
 class Class
+  include DaftFunkt
   def daft_funcs(*args)
     # We simply iterate through each passed in argument...
     args.each do |arg|
       # Makes sure that the arg is a valid daftFunkt
-      next unless DaftFunkt::Functions[arg]
+      next if DaftFunkt::Functions[arg].nil?
 
       # Here's the getter
-      class_eval("def #{arg};@#{arg};end")
-
-      # Here's the setter
-      class_eval("def #{arg}=();@#{arg}=#{DaftFunkt::Functions[arg]};end")
+      class_eval("def #{arg}; ::DaftFunkt::Functions[:#{arg}] ;end")
     end
   end
 end
